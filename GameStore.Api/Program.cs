63386d1fb2e3e0617139511445b848be941602a1
juliannparra99 <julianfranco.parra@gmail.com>
2024-04-07 19:08:29 -0300

    Added: 1.4- added asp.net core configuration system to handle the connString to the database + Generating the database and applying the migrations

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -3,12 +3,14 @@ using GameStore.Api.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
 
-//For production or if you wanna hanlde credentials, you should change de connString in other place for security; outside of the code base
-var connString = "Data Source=GameStore.db";
+var connString = builder.Configuration.GetConnectionString("GameStore");
+
 builder.Services.AddSqlite<GameStoreContext>(connString);
 
 var app = builder.Build();
 
 app.MapGamesEndpoints();
+
+app.MigrateDb();
 
 app.Run();
--- GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75a44c9e96f576d42176d3745b531c729cfac1b9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e737107a993a913f0fa89199f41e083a19fe5aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
