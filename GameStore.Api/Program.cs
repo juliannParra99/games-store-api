e7f86b811cd57468e64613d504df724bf2e1c4af
juliannparra99 <julianfranco.parra@gmail.com>
2024-05-01 12:47:20 -0300

    Added: 1.10.2 - 'AddJwtBearer (option) ' settings

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -2,6 +2,7 @@ using System.Text;
 using GameStore.Api.Data;
 using GameStore.Api.Endpoints;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -23,12 +24,26 @@ builder.Services.AddAuthentication(configureOptions: options =>
 
 
 })
-.AddJwtBearer();
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true, //sometime in local machine can  generate an error cause we're ussing http rather than https, so it could generate an error; so we could put it in false to solve it
+        ValidateAudience = true,
+        ValidateIssuerSigningKey = true,
+        ValidateLifetime = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JwtConfig:Secret"]!))
+    };
+});
+
 
 builder.Services.AddAuthorization();
-
-
 var app = builder.Build();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapGamesEndpoints();
 app.MapGenresEndpoints();
