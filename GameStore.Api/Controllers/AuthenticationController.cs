40679b346cfe2ec26be4fe04ee05073f82ce76f1
juliannparra99 <julianfranco.parra@gmail.com>
2024-05-05 19:05:14 -0300

    Added: 1.10.5- Registration: Adding registration (register action); next step will be add the token for the user

--- GameStore.Api/Controllers/AuthenticationController.cs
+++ GameStore.Api/Controllers/AuthenticationController.cs
@@ -4,9 +4,11 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using GameStore.Api.Configurations;
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using RouteAttribute = Microsoft.AspNetCore.Mvc.RouteAttribute;
 
 namespace GameStore.Api.Controllers
 {
@@ -26,5 +28,65 @@ namespace GameStore.Api.Controllers
             _jwtConfig = jwtConfig;
 
         }
+
+        // Registers a new user by checking if the provided email is not already in use.
+        // If the email is unique, creates a new user with the provided email and password.
+        // Returns a BadRequest response if the email is already in use or if there is a server error.
+        [HttpPost]
+        [Route("Register")]
+        public async Task<IActionResult> Register([FromBody] UserRegistrationRequestDto requestDto)
+        {
+            //validate the incoming request
+            if (ModelState.IsValid)
+            {
+                //we need to check if the email already exist
+                var user_exist = await _userManager.FindByEmailAsync(requestDto.Email);
+
+                if (user_exist != null)
+                {
+                    return BadRequest(new AuthResult()
+                    {
+                        Result = false,
+                        Errors = new List<string>()
+                        {
+                            "email already exist"
+
+                        }
+
+                    });
+                }
+
+                //create a user
+                var new_user = new IdentityUser()
+                {
+                    //data which we receive from user
+                    Email = requestDto.Email,
+                    UserName = requestDto.Email
+                };
+
+                var is_created = await _userManager.CreateAsync(new_user, requestDto.Password);
+
+                //check if it was created succesfully
+                if (is_created.Succeeded)
+                {
+                    //generate the token
+
+                }
+
+                //if is_created is not succeded, we're gonna return an error
+                return BadRequest(new AuthResult()
+                {
+                    Errors = new List<string>(){
+                        "Server error"
+                    },
+                    Result = false
+
+                });
+            }
+
+            return BadRequest();
+
+        }
+
     }
 }
